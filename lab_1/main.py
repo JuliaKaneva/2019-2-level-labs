9640f08555eb64650f597c39a3624185da558092
JuliaKaneva <ukaneva2356@gmail.com>
2019-09-21 16:04:17 +0300

    Added return to calculate_frequences function

--- lab_1/main.py
+++ lab_1/main.py
@@ -8,8 +8,7 @@ def calculate_frequences(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    a = 10
-    return a
+    return {'word': 10}
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     """
